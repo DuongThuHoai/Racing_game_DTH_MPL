bc4a40efc1ab22dd1cce91bcd3ba3b7bda93f016
Thu Hoài <74605956+DuongThuHoai@users.noreply.github.com>
2022-02-10 08:35:27 +0700

    update

---
+++ Racing_Game_DTH_MPL/Racing_Game/Racing_Game/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Racing_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pcb_code = new System.Windows.Forms.PictureBox();
+            this.pcb_bug2 = new System.Windows.Forms.PictureBox();
+            this.pcb_bug1 = new System.Windows.Forms.PictureBox();
+            this.pcb_bug3 = new System.Windows.Forms.PictureBox();
+            this.pcb_over = new System.Windows.Forms.PictureBox();
+            this.lb_tien1 = new System.Windows.Forms.Label();
+            this.lb_tien3 = new System.Windows.Forms.Label();
+            this.lb_tien2 = new System.Windows.Forms.Label();
+            this.lb_tien4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_code)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_bug2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_bug1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_bug3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_over)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(130, -45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 140);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(332, 208);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(10, 140);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(130, 208);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(10, 140);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox4.BackColor = System.Drawing.Color.White;
+            this.pictureBox4.Location = new System.Drawing.Point(332, -45);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(10, 140);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox5.BackColor = System.Drawing.Color.White;
+            this.pictureBox5.Location = new System.Drawing.Point(332, 447);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(10, 140);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox6.BackColor = System.Drawing.Color.White;
+            this.pictureBox6.Location = new System.Drawing.Point(130, 447);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(10, 140);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pcb_code
+            // 
+            this.pcb_code.Image = ((System.Drawing.Image)(resources.GetObject("pcb_code.Image")));
+            this.pcb_code.Location = new System.Drawing.Point(200, 416);
+            this.pcb_code.Name = "pcb_code";
+            this.pcb_code.Size = new System.Drawing.Size(40, 40);
+            this.pcb_code.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb_code.TabIndex = 6;
+            this.pcb_code.TabStop = false;
+            // 
+            // pcb_bug2
+            // 
+            this.pcb_bug2.Image = ((System.Drawing.Image)(resources.GetObject("pcb_bug2.Image")));
+            this.pcb_bug2.Location = new System.Drawing.Point(164, 94);
+            this.pcb_bug2.Name = "pcb_bug2";
+            this.pcb_bug2.Size = new System.Drawing.Size(60, 63);
+            this.pcb_bug2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb_bug2.TabIndex = 7;
+            this.pcb_bug2.TabStop = false;
+            // 
+            // pcb_bug1
+            // 
+            this.pcb_bug1.Image = ((System.Drawing.Image)(resources.GetObject("pcb_bug1.Image")));
+            this.pcb_bug1.Location = new System.Drawing.Point(348, 170);
+            this.pcb_bug1.Name = "pcb_bug1";
+            this.pcb_bug1.Size = new System.Drawing.Size(60, 63);
+            this.pcb_bug1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb_bug1.TabIndex = 8;
+            this.pcb_bug1.TabStop = false;
+            // 
+            // pcb_bug3
+            // 
+            this.pcb_bug3.Image = ((System.Drawing.Image)(resources.GetObject("pcb_bug3.Image")));
+            this.pcb_bug3.Location = new System.Drawing.Point(30, 360);
+            this.pcb_bug3.Name = "pcb_bug3";
+            this.pcb_bug3.Size = new System.Drawing.Size(60, 63);
+            this.pcb_bug3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb_bug3.TabIndex = 9;
+            this.pcb_bug3.TabStop = false;
+            // 
+            // pcb_over
+            // 
+            this.pcb_over.Image = ((System.Drawing.Image)(resources.GetObject("pcb_over.Image")));
+            this.pcb_over.Location = new System.Drawing.Point(130, 0);
+            this.pcb_over.Name = "pcb_over";
+            this.pcb_over.Size = new System.Drawing.Size(237, 50);
+            this.pcb_over.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcb_over.TabIndex = 10;
+            this.pcb_over.TabStop = false;
+            // 
+            // lb_tien1
+            // 
+            this.lb_tien1.AutoSize = true;
+            this.lb_tien1.BackColor = System.Drawing.Color.Yellow;
+            this.lb_tien1.Location = new System.Drawing.Point(58, 107);
+            this.lb_tien1.Name = "lb_tien1";
+            this.lb_tien1.Size = new System.Drawing.Size(21, 20);
+            this.lb_tien1.TabIndex = 0;
+            this.lb_tien1.Text = "O";
+            // 
+            // lb_tien3
+            // 
+            this.lb_tien3.AutoSize = true;
+            this.lb_tien3.BackColor = System.Drawing.Color.Yellow;
+            this.lb_tien3.Location = new System.Drawing.Point(239, 341);
+            this.lb_tien3.Name = "lb_tien3";
+            this.lb_tien3.Size = new System.Drawing.Size(21, 20);
+            this.lb_tien3.TabIndex = 11;
+            this.lb_tien3.Text = "O";
+            // 
+            // lb_tien2
+            // 
+            this.lb_tien2.AutoSize = true;
+            this.lb_tien2.BackColor = System.Drawing.Color.Yellow;
+            this.lb_tien2.Location = new System.Drawing.Point(88, 223);
+            this.lb_tien2.Name = "lb_tien2";
+            this.lb_tien2.Size = new System.Drawing.Size(21, 20);
+            this.lb_tien2.TabIndex = 12;
+            this.lb_tien2.Text = "O";
+            // 
+            // lb_tien4
+            // 
+            this.lb_tien4.AutoSize = true;
+            this.lb_tien4.BackColor = System.Drawing.Color.Yellow;
+            this.lb_tien4.Location = new System.Drawing.Point(397, 360);
+            this.lb_tien4.Name = "lb_tien4";
+            this.lb_tien4.Size = new System.Drawing.Size(21, 20);
+            this.lb_tien4.TabIndex = 13;
+            this.lb_tien4.Text = "O";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Money = ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(478, 544);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lb_tien4);
+            this.Controls.Add(this.lb_tien2);
+            this.Controls.Add(this.lb_tien3);
+            this.Controls.Add(this.lb_tien1);
+            this.Controls.Add(this.pcb_over);
+            this.Controls.Add(this.pcb_bug3);
+            this.Controls.Add(this.pcb_bug1);
+            this.Controls.Add(this.pcb_bug2);
+            this.Controls.Add(this.pcb_code);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Racing Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_code)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_bug2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_bug1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_bug3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_over)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pcb_code;
+        private System.Windows.Forms.PictureBox pcb_bug2;
+        private System.Windows.Forms.PictureBox pcb_bug1;
+        private System.Windows.Forms.PictureBox pcb_bug3;
+        private System.Windows.Forms.PictureBox pcb_over;
+        private System.Windows.Forms.Label lb_tien1;
+        private System.Windows.Forms.Label lb_tien3;
+        private System.Windows.Forms.Label lb_tien2;
+        private System.Windows.Forms.Label lb_tien4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Racing_Game_DTH_MPL/Racing_Game/Racing_Game/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Racing_game
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            pcb_over.Visible = false;
+        }
+
+        int gamespeed = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            move(gamespeed);//toc do cua tro choi
+            bug(gamespeed);//toc do chay cua bug
+            gameover();
+            tien(gamespeed);
+            laytien();
+        }
+
+        int antien;
+        Random r = new Random();
+        int x;
+
+        void bug(int speed)
+        {
+            if (pcb_bug1.Top >= 500)
+            {
+                x = r.Next(0, 200);//vi tri cua bug1 duoc chay trong khoang(0,200)
+                pcb_bug1.Location = new Point(x, 0);
+            }
+            else
+                pcb_bug1.Top += speed;
+
+            if (pcb_bug2.Top >= 500)
+            {
+                x = r.Next(180, 260);//bug2 chay tu (180, 260)
+                pcb_bug2.Location = new Point(x, 0);
+            }
+            else
+                pcb_bug2.Top += speed;
+
+            if (pcb_bug3.Top >= 500)
+            {
+                x = r.Next(0, 80);//vi tri cua bug3
+                pcb_bug3.Location = new Point(x, 0);
+            }
+            else
+                pcb_bug3.Top += speed;
+        }
+
+        void tien(int speed)
+        {
+
+            if (lb_tien1.Top >= 500)
+            {
+                x = r.Next(0, 200);
+                lb_tien1.Location = new Point(x, 0);
+            }
+            else
+                lb_tien1.Top += speed;
+
+            if (lb_tien2.Top >= 500)
+            {
+                x = r.Next(0, 200);
+                lb_tien2.Location = new Point(x, 0);
+            }
+            else
+                lb_tien2.Top += speed;
+
+            if (lb_tien3.Top >= 500)
+            {
+                x = r.Next(0, 200);
+                lb_tien3.Location = new Point(x, 0);
+            }
+            else
+                lb_tien3.Top += speed;
+
+            if (lb_tien4.Top >= 500)
+            {
+                x = r.Next(0, 200);
+                lb_tien4.Location = new Point(x, 0);
+            }
+            else
+                lb_tien4.Top += speed;
+        }
+
+        void gameover()
+        {
+
+            if (pcb_code.Bounds.IntersectsWith(pcb_bug1.Bounds))
+            {
+                timer1.Enabled = false;
+                pcb_over.Visible = true;
+                DialogResult result = MessageBox.Show("Bạn muốn chơi lại chứ ?",
+                    "Hii, it's me again!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                switch (result)
+                {
+                    case DialogResult.Yes:
+                        MessageBox.Show("Okey, Let's go !");
+                        Application.Restart();
+                        break;
+                    case DialogResult.No:
+                        MessageBox.Show("Game Over!");
+                        this.Close();
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            if (pcb_code.Bounds.IntersectsWith(pcb_bug2.Bounds))
+            {
+                timer1.Enabled = false;
+                pcb_over.Visible = true;
+                DialogResult result = MessageBox.Show("Bạn muốn chơi lại chứ ?",
+                    "Hii, it's me again!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                switch (result)
+                {
+                    case DialogResult.Yes:
+                        MessageBox.Show("Okey, Let's go !");
+                        Application.Restart();
+                        break;
+                    case DialogResult.No:
+                        MessageBox.Show("Game over!");
+                        this.Close();
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            if (pcb_code.Bounds.IntersectsWith(pcb_bug3.Bounds))
+            {
+                timer1.Enabled = false;
+                pcb_over.Visible = true;
+                DialogResult result = MessageBox.Show("Bạn muốn chơi lại chứ ?",
+                    "Hii, it's me again!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                switch (result)
+                {
+                    case DialogResult.Yes:
+                        MessageBox.Show("Okey, Let's go !");
+                        Application.Restart();
+                        break;
+                    case DialogResult.No:
+                        MessageBox.Show("Game Over!");
+                        this.Close();
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+
+        void move(int speed)// goi ham de di chuyen cac picturebox1,2,3,4
+        {
+            if (pictureBox1.Top >= 500)
+                pictureBox1.Top = 0;
+            else
+                pictureBox1.Top += speed;
+
+            if (pictureBox2.Top >= 500)
+                pictureBox2.Top = 0;
+            else
+                pictureBox2.Top += speed;
+
+            if (pictureBox3.Top >= 500)
+                pictureBox3.Top = 0;
+            else
+                pictureBox3.Top += speed;
+
+            if (pictureBox4.Top >= 500)
+                pictureBox4.Top = 0;
+            else
+                pictureBox4.Top += speed;
+
+            if (pictureBox5.Top >= 500)
+                pictureBox5.Top = 0;
+            else
+                pictureBox5.Top += speed;
+
+            if (pictureBox6.Top >= 500)
+                pictureBox6.Top = 0;
+            else
+                pictureBox6.Top += speed;
+        }
+
+        void laytien()
+        {
+            if (pcb_code.Bounds.IntersectsWith(lb_tien1.Bounds))
+            {
+                antien++;
+                label1.Text = "Money" + antien.ToString();
+                x = r.Next(0, 200);
+                lb_tien1.Location = new Point(x, 0);
+            }
+
+            if (pcb_code.Bounds.IntersectsWith(lb_tien2.Bounds))
+            {
+                antien++;
+                label1.Text = "Money = " + antien.ToString();
+                x = r.Next(0, 200);//day chi la vi tri mac dinh
+                lb_tien2.Location = new Point(x, 0);
+            }
+
+            if (pcb_code.Bounds.IntersectsWith(lb_tien3.Bounds))
+            {
+                antien++;
+                label1.Text = "Money = " + antien.ToString();
+                x = r.Next(0, 200);
+                lb_tien3.Location = new Point(x, 0);
+            }
+
+            if (pcb_code.Bounds.IntersectsWith(lb_tien4.Bounds))
+            {
+                antien++;
+                label1.Text = "Money = " + antien.ToString();
+                x = r.Next(0, 200);
+                lb_tien4.Location = new Point(x, 0);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                if (pcb_code.Left > 1)// muc chan de khong vuot qua man hinh ben trai
+                    pcb_code.Left += -8;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if (pcb_code.Right < 315)// "code" khong vuot ra man hinh ben phai
+                    pcb_code.Left += 8;
+            }
+            if (e.KeyCode == Keys.Up)//nhan up de dieu chinh toc do nhanh hon
+            {
+                if (gamespeed < 21)//muc toc do toi da la 21
+                    gamespeed++;
+            }
+            if (e.KeyCode == Keys.Down)// nhan down de giam toc do xuong
+                if (gamespeed > 1)
+                    gamespeed--;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            DialogResult result = MessageBox.Show("Start", "Hi EveryOne, I'm box and" +
+                " have a nice day!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            switch (result)
+            {
+                case DialogResult.No:
+                    MessageBox.Show("Bạn có chắc muốn thoát chứ !");
+                    this.Close();
+                    break;
+                case DialogResult.Yes:
+                    MessageBox.Show("Bắt đầu thôi nào!");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+    }
+}
